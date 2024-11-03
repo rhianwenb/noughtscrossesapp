cbf0921622c63c30a4654e13c431aa9bdd9690a4
Rhianwen Beint <156092630+rhianwenb@users.noreply.github.com>
2024-11-03 15:22:40 +0000

    added click functionality to squares

--- client/components/Square.jsx
+++ client/components/Square.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 
 import '../styles/Square.css';
 
-export default function Square({value}) {
+export default function Square() {
 
+    const [value, setValue] = useState(null)
+
+
+    function handleClick() {
+        setValue("X");
+    };
 
   return (
     <div>
         
-        <button className="square"> {value} </button>
+        <button className="square"
+        onClick={handleClick} > 
+            {value} 
+        </button>
 
     </div>
   )
